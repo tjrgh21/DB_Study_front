316735accd54cd4acd9242a411532910b6d90b5e
MinGyu Lim <alsrb2277@naver.com>
2025-02-05 17:36:00 +0900

    style: Fix button style

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -52,56 +52,59 @@ function Login() {
         })
     }
 
-return (
-    <div className="Login">
-        <h3>Sign in</h3>
-        <div className="inputContainer">
-            <input
-                type="text"
-                placeholder="ID"
-                onChange={onChangeSignup}
-                name="id"
-                value={userSignup.id}
-            />
-            <br />
-            <input
-                type="password"
-                placeholder="PW"
-                onChange={onChangeSignup}
-                name="pw"
-                value={userSignup.pw}
-            />
-            <br />
-            <input
-                type="text"
-                placeholder="name"
-                onChange={onChangeSignup}
-                name="name"
-                value={userSignup.name}
-            />
+    return (
+        <div className="Login">
+            <h3>로그인</h3>
+            <div className="LoginContainer">
+                <input
+                    type="text"
+                    placeholder="ID"
+                    onChange={onChangeLogin}
+                    name="id"
+                    value={userLogin.id}
+                />
+                <br />
+                <input
+                    type="password"
+                    placeholder="PW"
+                    onChange={onChangeLogin}
+                    name="pw"
+                    value={userLogin.pw}
+                />
+            </div>
+            <div className="LogBtns">
+            <button className="LogBtn" onClick={onClickLogin}>로그인</button>
+            <button className="LogBtn" onClick={onClickLogout}>로그아웃</button>
+            </div>
+            <h3>회원가입</h3>
+            <div className="SignupContainer">
+                <input
+                    type="text"
+                    placeholder="ID"
+                    onChange={onChangeSignup}
+                    name="id"
+                    value={userSignup.id}
+                />
+                <br />
+                <input
+                    type="password"
+                    placeholder="PW"
+                    onChange={onChangeSignup}
+                    name="pw"
+                    value={userSignup.pw}
+                />
+                <br />
+                <input
+                    type="text"
+                    placeholder="Name"
+                    onChange={onChangeSignup}
+                    name="name"
+                    value={userSignup.name}
+                />
+            </div>
+            <button className="SignupBtn" onClick={onClickSignup}>회원가입</button>
         </div>
-        <button onClick={onClickSignup}>회원가입</button>
-        <div>
-            <input
-                type="text"
-                placeholder="ID"
-                onChange={onChangeLogin}
-                name="id"
-                value={userLogin.id}
-            />
-            <br />
-            <input
-                type="password"
-                placeholder="PW"
-                onChange={onChangeLogin}
-                name="pw"
-                value={userLogin.pw}
-            />
-        </div>
-        <button onClick={onClickLogin}>로그인</button>
-        <button onClick={onClickLogout}>로그아웃</button>
-    </div>
-)
+    )
 };
 
 export default Login;
